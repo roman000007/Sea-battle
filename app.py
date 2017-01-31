6340906cca9b96fc8eab9b376e38813a8e3378b3
Roman <roman.vey@gmail.com>
2017-01-31 16:18:54 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+def read_field(file):
+    res = []
+    with open(file + ".txt", "r") as fl:
+        text = fl.read().split("\n")
+    for row in text:
+        tmp = []
+        for ch in row:
+            tmp.append(ch)
+        res.append(tmp)
+    return res
+
+field = read_field("field")
+
+#print(field)
+
+def has_ship(field, coords):
+    """
+    :param field:
+    :param coords: which row, which elem
+    :return: is ship there
+    """
+    ind_1 = ord(coords[0]) - ord('A')
+    ind_2 = int(coords[1]) - 1
+    return field[ind_1][ind_2] == "*"
+
+#print(has_ship(field, ('A','1')))
